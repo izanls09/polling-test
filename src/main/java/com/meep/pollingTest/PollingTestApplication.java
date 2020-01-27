f266176a42fd5e26965f9b5eca61dc364499210c
Izan <IzanLS09@gmail.com>
2020-01-27 14:47:10 +0100

    Base archetype

---
+++ src/main/java/com/meep/pollingTest/PollingTestApplication.java
@@ -0,0 +1,13 @@
+package com.meep.pollingTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollingTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollingTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meep/pollingTest/PollingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meep.pollingTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollingTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
